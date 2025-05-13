42ff7566ba5bd946c210267a23249724625af9bd
Tyler Onstad-Cran <tonstadcran@gmail.com>
2025-05-13 13:40:51 +0200

    initial commit

---
+++ Code/01 Código proporcionado/CODIGO_BASE_ALUMNOS/Formulas.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def euclidean_dist(x1, y1, x2, y2):
+    return ((x1 - x2) **2 + (y1 - y2) **2) ** .5
+
+
+def manhattan_dist(x1, y1, x2, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
